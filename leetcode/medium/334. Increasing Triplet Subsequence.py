08260b79b4de62bfb2090bfab84e0b70e6585529
withham <withham1@naver.com>
2020-01-02 21:52:11 +0900

    Create 334. Increasing Triplet Subsequence.py

---
+++ leetcode/medium/334. Increasing Triplet Subsequence.py
@@ -0,0 +1,19 @@
+# problem : https://leetcode.com/problems/increasing-triplet-subsequence/
+# time complexity : O(N)
+
+import sys
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+        one, two = nums[0], sys.maxsize
+        for three in nums[1:]:
+            if three > two:
+                return True
+            if three > one and three < two:
+                two = three
+            if three < one:
+                one = three
+
+        return False
