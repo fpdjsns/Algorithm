314fa1b3f78afcfb32527d056c1ff50836522350
withham <withham1@naver.com>
2020-02-06 22:35:35 +0900

    Create 1192. Critical Connections in a Network.cpp

---
+++ leetcode/hard/1192. Critical Connections in a Network.cpp
@@ -0,0 +1,53 @@
+/*
+ * 문제 : https://leetcode.com/problems/critical-connections-in-a-network/
+ * 시간복잡도 : O(V+E)
+ * 알고리즘 : tarjan
+ */
+ 
+class Solution {
+public:
+
+    vector<vector<int>> edge;
+    vector<int> num;
+    vector<int> low;
+    vector<vector<int>> ans;
+
+    int t = 1;
+    int tarjan(int now, int before) {
+        int& ret = low[now];
+        num[now] = t++;
+        for (int i = 0; i<edge[now].size(); i++) {
+            int next = edge[now][i];
+
+            if (next == before) continue;
+            if (num[next] == -1) { // not visit yet
+                ret = min(ret, tarjan(next, now));
+
+                if (low[next] > num[now]) {
+                    ans.push_back({ now, next });
+                }
+            } else {
+                ret = min(ret, num[next]);
+            }
+        }
+        return ret;
+    }
+
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+
+        edge = vector<vector<int>>(n);
+        num = vector<int>(n, -1);
+        low = vector<int>(n, n);
+
+		for (int i = 0; i<connections.size(); i++) {
+          int a = connections[i][0];
+          int b = connections[i][1];
+          edge[a].push_back(b);
+          edge[b].push_back(a);
+        }
+
+        tarjan(0, -1);
+
+        return ans;
+    }
+};
