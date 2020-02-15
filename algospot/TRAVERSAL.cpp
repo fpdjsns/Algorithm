a40ad53594581734118d38263607ef3d89a3545c
withham <withham1@naver.com>
2020-02-15 11:25:53 +0900

    Update TRAVERSAL.cpp

--- algospot/TRAVERSAL.cpp
+++ algospot/TRAVERSAL.cpp
@@ -1,8 +1,8 @@
 /**
- * problem : https://algospot.com/judge/problem/read/TRAVERSAL
- * time complexity : 
- * data structure : tree
- */
+* problem : https://algospot.com/judge/problem/read/TRAVERSAL
+* time complexity : O(N^2)
+* data structure : tree
+*/
 
 #include <string>
 #include <set>
@@ -12,36 +12,35 @@
 
 using namespace std;
 
-void solve(vector<int> preArr, vector<int> inArr) {
+// arr[begin, end)
+vector<int> getSubVector(vector<int> arr, int begin, int end) {
+	return vector<int>(arr.begin() + begin, arr.begin() + end);
+}
+
+void postOrder(vector<int> preArr, vector<int> inArr) {
 	int N = preArr.size();
 	if (N == 1) cout << preArr[0] << " ";
 	if (N <= 1) return;
 
+	// 루트는 전위순회했을 때 가장 먼저 나오는 노드
 	int root = preArr[0];
+
+	// 왼쪽 자식 노드들이 없는 경우
 	if (inArr[0] == preArr[0]) {
-		vector<int> nextPreArr = vector<int>(N-1);
-		vector<int> nextInArr = vector<int>(N-1);
-		for (int i = 1; i < N; i++) nextPreArr[i-1] = preArr[i];
-		for (int i = 1; i < N; i++) nextInArr[i-1] = inArr[i];
-		solve(nextPreArr, nextInArr);
+		// 다음 배열들부터 다시 탐색
+		postOrder(getSubVector(preArr, 1, N), getSubVector(inArr, 1, N));
 	}
 	else {
-		int nextN = 0;
-		while (nextN < N && inArr[nextN] != root) nextN++;
-		vector<int> nextPreArr = vector<int>(nextN);
-		vector<int> nextInArr = vector<int>(nextN);
-		for (int i = 1; i <= nextN; i++) nextPreArr[i - 1] = preArr[i];
-		for (int i = 0; i < nextN; i++) nextInArr[i] = inArr[i];
-		solve(nextPreArr, nextInArr);
+		// 중위 순회 배열에서 root 노드 인덱스 = 왼쪽 자식 노드들의 개수
+		int inArrRootInd = 0;
+		while (inArrRootInd < N && inArr[inArrRootInd] != root) inArrRootInd++;
 
-		nextPreArr = vector<int>(N - nextN - 1);
-		nextInArr = vector<int>(N - nextN - 1);
-		for (int i = nextN + 1; i < N; i++) {
-			nextPreArr[i - nextN - 1] = preArr[i];
-			nextInArr[i - nextN - 1] = inArr[i];
-		}
-		solve(nextPreArr, nextInArr);
+		// 1. 왼쪽 자식노드들 후위 순회 = 왼쪽 자식 노드 방문
+		postOrder(getSubVector(preArr, 1, inArrRootInd + 1), getSubVector(inArr, 0, inArrRootInd));
+		// 2. 오른쪽 자식노드들 후위 순회 = 오른쪽 자식 노드 방문
+		postOrder(getSubVector(preArr, inArrRootInd + 1, N), getSubVector(inArr, inArrRootInd + 1, N));
 	}
+	// 3. 루트 방문
 	cout << root << " ";
 }
 
@@ -54,7 +53,7 @@ int main() {
 		vector<int> inArr(N);
 		for (int i = 0; i < N; i++) cin >> preArr[i];
 		for (int i = 0; i < N; i++) cin >> inArr[i];
-		solve(preArr, inArr);
+		postOrder(preArr, inArr);
 		cout << endl;
 	}
 	return 0;
