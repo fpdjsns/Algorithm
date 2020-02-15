fc9be3793599125b7ba32889f91cdaa09b5362f2
withham <withham1@naver.com>
2020-02-15 12:39:31 +0900

    Create 1329. Sort the Matrix Diagonally.cpp

---
+++ leetcode/medium/1329. Sort the Matrix Diagonally.cpp
@@ -0,0 +1,28 @@
+/**
+ * problem: https://leetcode.com/problems/sort-the-matrix-diagonally/
+ * time complexity: O(NlogN)
+ * data structure: heap
+ */
+ 
+class Solution {
+public:
+	vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+		int m = mat.size();
+		int n = mat[0].size();
+		vector<priority_queue<int>> heapArr(m + n - 1);
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				heapArr[j - i + m - 1].push(-mat[i][j]);
+            }
+		}
+
+		for (int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++){
+				mat[i][j] = -heapArr[j - i + m - 1].top();
+				heapArr[j - i + m - 1].pop();
+			}
+		}
+		return mat;
+	}
+};
