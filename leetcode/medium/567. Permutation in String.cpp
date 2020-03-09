ddccd2e7447a9660d6023ab7a96c2137dc55f8af
withham <withham1@naver.com>
2020-03-10 00:58:40 +0900

    Create 567. Permutation in String.cpp

---
+++ leetcode/medium/567. Permutation in String.cpp
@@ -0,0 +1,27 @@
+/**
+ * problem : https://leetcode.com/problems/permutation-in-string/
+ * algorithm: sliding window
+ * time complexity: O(|s1| + |s2|)
+ */
+ 
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> m(26, 0);
+        for (int i = 0; i<s1.size(); i++) m[s1[i] - 'a']++;
+        int s = 0;
+        for (int e = 0; e<s2.size(); e++) {
+            int lastChar = s2[e] - 'a';
+            if (m[lastChar] <= 0) {
+                while (s < e && s2[s] - 'a' != lastChar) {
+                    m[s2[s++] - 'a']++;
+                }
+                s++;
+                continue;
+            }
+            m[lastChar]--;
+            if (e - s == s1.size() - 1) return true;
+        }
+        return false;
+    }
+};
