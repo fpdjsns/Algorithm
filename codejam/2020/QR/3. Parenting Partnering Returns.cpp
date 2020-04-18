2ca82bfeb06083bf11814ab3d8c134383cbbf91e
withham <withham1@naver.com>
2020-04-18 11:53:27 +0900

    Update 3. Parenting Partnering Returns.cpp

--- codejam/2020/QR/3. Parenting Partnering Returns.cpp
+++ codejam/2020/QR/3. Parenting Partnering Returns.cpp
@@ -1,6 +1,6 @@
 /**
  * problem : https://codingcompetitions.withgoogle.com/codejam/round/000000000019fd27/000000000020bdf9
- * time complexity : O(N)
+ * time complexity : O(NlogN)
  * algorithm : greedy
  */
  
