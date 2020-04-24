78c138f3b7386150fed296a08da7f21bfe4b3229
withham <withham1@naver.com>
2020-04-24 18:06:58 +0900

    Create 201. Bitwise AND of Numbers Range.cpp

---
+++ leetcode/medium/201. Bitwise AND of Numbers Range.cpp
@@ -0,0 +1,16 @@
+/**
+ * problem : https://leetcode.com/problems/bitwise-and-of-numbers-range/
+ * time complexity : O(logN). N = min(m, n).
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int cnt = 0;
+        while(m < n) {
+            m = m >> 1;
+            n = n >> 1;
+            cnt++;
+        }
+        return n<<cnt;
+    }
+};
