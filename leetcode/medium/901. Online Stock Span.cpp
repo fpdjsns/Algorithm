2f562a592155b0afb6e232cc1ceea46b0df71892
withham <withham1@naver.com>
2020-05-19 19:27:48 +0900

    Create 901. Online Stock Span.cpp

---
+++ leetcode/medium/901. Online Stock Span.cpp
@@ -0,0 +1,27 @@
+/**
+ * problem : https://leetcode.com/problems/online-stock-span/
+ */
+
+class StockSpanner {
+    vector<pair<int,int>> arr; // val, maxInd
+public:
+    StockSpanner() {
+        arr = vector<pair<int,int>>();
+    }
+    
+    int next(int price) {
+        int ind = arr.size()-1;
+        while(ind >= 0 && arr[ind].first <= price) {
+            ind = arr[ind].second;
+        }
+        int ans = arr.size() - ind;
+        arr.push_back({price, arr.size() - ans});
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
