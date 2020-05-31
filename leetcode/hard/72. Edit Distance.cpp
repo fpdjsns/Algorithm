3caedf41d830202daf0fb66939809e62e25bf457
withham <withham1@naver.com>
2020-05-31 19:17:59 +0900

    Create 72. Edit Distance.cpp

---
+++ leetcode/hard/72. Edit Distance.cpp
@@ -0,0 +1,30 @@
+/**
+ * problem : https://leetcode.com/problems/edit-distance/
+ * algorithm : DP
+ * time complexity : O(NM)
+ */
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size() + 1));
+        
+        for(int i=1;i<=word1.size();i++)
+            dp[i][0] = i;
+        for(int i=1;i<=word2.size();i++)
+            dp[0][i] = i;
+        
+        for(int i=1;i<=word1.size();i++){
+            for(int j=1;j<=word2.size();j++){
+                if(word1[i-1] == word2[j-1]) 
+                    dp[i][j] = dp[i-1][j-1];
+                else{
+                    dp[i][j] = min(dp[i][j-1], dp[i-1][j]);
+                    dp[i][j] = min(dp[i][j], dp[i-1][j-1]);
+                    dp[i][j]++;
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
+    }
+};
