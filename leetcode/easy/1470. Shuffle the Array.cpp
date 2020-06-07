23596449a8c15faac102894c58e20dce41f33c8c
withham <withham1@naver.com>
2020-06-07 13:09:36 +0900

    Create 1470. Shuffle the Array.cpp

---
+++ leetcode/easy/1470. Shuffle the Array.cpp
@@ -0,0 +1,16 @@
+/**
+ * problem : https://leetcode.com/problems/shuffle-the-array/
+ * time complexity : O(N)
+ */
+ 
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(2*n);
+        for(int i=0;i<n;i++){
+            ans[2*i] = nums[i];
+            ans[2*i+1] = nums[n+i]; 
+        }
+        return ans;
+    }
+};
