4c2f3d430498b2712370f8fd276273b54868eec8
withham <withham1@naver.com>
2020-06-08 19:40:19 +0900

    Create 231. Power of Two.cpp

---
+++ leetcode/easy/231. Power of Two.cpp
@@ -0,0 +1,16 @@
+/** 
+ * problem : https://leetcode.com/problems/power-of-two/
+ * time complexity : O(logN)
+ */
+ 
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        while(n > 0) {
+            if(n == 1) return true;
+            if(n%2 != 0) return false;
+            n /= 2;
+        }
+        return false;
+    }
+};
