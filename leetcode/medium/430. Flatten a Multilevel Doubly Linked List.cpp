acf99f9040ebc8db661f73afa1b58f05ba4fe31f
withham <withham1@naver.com>
2020-07-10 23:01:15 +0900

    Create 430. Flatten a Multilevel Doubly Linked List.cpp

---
+++ leetcode/medium/430. Flatten a Multilevel Doubly Linked List.cpp
@@ -0,0 +1,43 @@
+/**
+ * problem : https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+ * time complexity : O(N)
+ */
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+    Node* before;
+    void dfs(Node* now) {
+        if(now == nullptr) 
+            return;
+        
+        Node* next = now->next;
+        Node* child = now->child;
+        
+        if(before != nullptr){
+            now->prev = before;
+            before->next = now;
+        }
+        now->child = nullptr;
+        
+        before = now;
+        
+        dfs(child);
+        dfs(next);
+    }
+public:
+    Node* flatten(Node* head) {
+        before = nullptr;
+        dfs(head);
+        return head;
+    }
+};
