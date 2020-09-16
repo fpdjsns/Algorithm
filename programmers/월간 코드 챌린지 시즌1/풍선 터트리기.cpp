0922b11548ceb0e90bc8300f730b47cf71db9a55
withham <withham1@naver.com>
2020-09-17 07:59:16 +0900

    Create 풍선 터트리기.cpp

---
+++ programmers/월간 코드 챌린지 시즌1/풍선 터트리기.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> a) {
+   int size = a.size();
+   vector<int> leftMin(size);
+   vector<int> rightMin(size);
+   leftMin[0] = a[0];
+   rightMin[a.size() - 1] = a[size - 1];
+   for (int i = 0; i<size; i++) {
+      leftMin[i] = min(a[i], leftMin[max(0, i - 1)]);
+   }
+   for (int i = a.size() - 1; i >= 0; i--) {
+      rightMin[i] = min(a[i], rightMin[min(size - 1, i + 1)]);
+   }
+
+   int answer = 0;
+   for (int i = 0; i<a.size(); i++) {
+      int tmp = 0;
+      int now = a[i];
+      tmp += leftMin[max(0, i - 1)] < now;
+      tmp += rightMin[min(size - 1, i + 1)] < now;
+      if (tmp < 2) answer++;
+   }
+
+   return answer;
+}
