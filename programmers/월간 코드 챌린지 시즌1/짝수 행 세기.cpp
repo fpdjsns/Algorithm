320f08e5cc4c658085470260d76d356cf58ebf06
withham <withham1@naver.com>
2020-09-20 17:13:21 +0900

    Update 짝수 행 세기.cpp

--- programmers/월간 코드 챌린지 시즌1/짝수 행 세기.cpp
+++ programmers/월간 코드 챌린지 시즌1/짝수 행 세기.cpp
@@ -39,19 +39,19 @@ int solution(vector<vector<int>> arr) {
     dp[1][n - oneCnts[1]] = getCombination(n, oneCnts[1]);
     
     for(int column = 1; column <= m; column++){
-        int nowOneCnt = oneCnts[column]; // arr 배열에서 column 열의 1의 개수
-        for (int beforeEvenCnt = 0; beforeEvenCnt <= n; beforeEvenCnt++){ // 이때까지 세팅한 짝수 행의 개수
+        int oneCnt = oneCnts[column]; // arr 배열에서 column 열의 1의 개수
+        for (int num = 0; num <= n; num++){ // 이때까지 세팅한 짝수 행의 개수
             // 기존에 있던 행 중에 짝수개의 1을 가진 행에 1을 세팅하고자 하는 개수. 가전
-            for (int willSetEvenRows = 0; willSetEvenRows <= nowOneCnt; willSetEvenRows++) { 
+            for (int k = 0; k <= oneCnt; k++) { 
                 // 기존에 있던 행 중 홀수개의 1을 가진 행에 1을 세팅하고자 하는 개수. = column 열이 가진 1의 개수 중 짝수개를 가진 행에 세팅할 1을 뺀 나머지.
-                int willSetOddRow = nowOneCnt - willSetEvenRows; 
+                int willSetOddRow = oneCnt - k; 
                 // 1을 세팅하고 난 뒤 1을 짝수개 가진 행의 개수 = 짝수행 중 1을 세팅하지 않는 행 + 홀수행 중 1을 세팅하는 행
-                int willBeEvenRowCnt = (beforeEvenCnt - willSetEvenRows) + willSetOddRow;
+                int willBeEvenRowCnt = (num - k) + willSetOddRow;
                 if (willSetOddRow < 0 || willBeEvenRowCnt > n || willBeEvenRowCnt < 0) continue; // IMPOSSIBLE
                 
                 // 경우의 수 = 짝수행에 1을 세팅하는 경우의 수 * 홀수행에 1을 세팅하는 경우의 수 % MOD
-                long long numOfCase = (getCombination(beforeEvenCnt, willSetEvenRows) * getCombination(n - beforeEvenCnt, willSetOddRow)) % MOD;
-                dp[column][willBeEvenRowCnt] += dp[column-1][beforeEvenCnt] * numOfCase % MOD;
+                long long numOfCase = (getCombination(num, k) * getCombination(n - num, oneCnt - k)) % MOD;
+                dp[column][willBeEvenRowCnt] += dp[column-1][num] * numOfCase % MOD;
                 dp[column][willBeEvenRowCnt] %= MOD;
             }
         }
