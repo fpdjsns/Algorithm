b03e5fad46084f4b40c4e074068b63e6e90bc2b8
withham <withham1@naver.com>
2020-11-07 12:21:10 +0900

    Create 이진 변환 반복하기.cpp

---
+++ programmers/월간 코드 챌린지 시즌1/이진 변환 반복하기.cpp
@@ -0,0 +1,35 @@
+/**
+ * problem : https://programmers.co.kr/learn/courses/30/lessons/70129
+ * time complexity : O((logN)^2)
+ */
+ 
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string changeBinary(int num) {
+    string binary;
+    while(num > 0) {
+        binary.push_back((num % 2) + '0');
+        num /= 2;
+    }
+    reverse(binary.begin(), binary.end());
+    return binary;
+}
+
+vector<int> solution(string s) {
+    int cnt = 0;
+    int zeroCnt = 0;
+    while(s.size() != 1) {
+        cnt++;
+        int tmpCnt = 0;
+        for(int i=0;i<s.size();i++){
+            tmpCnt += s[i] == '0';
+        }
+        zeroCnt += tmpCnt;
+        s = changeBinary(s.size() - tmpCnt);
+    }
+    return vector<int> { cnt, zeroCnt };
+}
