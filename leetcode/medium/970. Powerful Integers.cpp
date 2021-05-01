dc1cc84407e1bd240b7a52edc1a0155de3f7d5cf
withham <withham1@naver.com>
2021-05-01 10:33:03 +0900

    Create 970. Powerful Integers.cpp

---
+++ leetcode/medium/970. Powerful Integers.cpp
@@ -0,0 +1,24 @@
+/**
+ * problem : https://leetcode.com/problems/powerful-integers/
+ * time complexity : O(logN * logM) // N=x, M=y
+ */
+ 
+class Solution {
+public:
+    vector<int> powerfulIntegers(int x, int y, int bound) {
+        set<int> dict;
+        for(int a = 1; a < bound; a *= x){
+            for(int b = 1; b < bound; b *= y){
+                if(a + b > bound) break;
+                dict.insert(a + b);
+                if(y == 1) break;
+            }
+            if(x == 1) break;
+        }
+        vector<int> answer;
+        for(auto num: dict) {
+            answer.push_back(num);
+        }
+        return answer;
+    }
+};
