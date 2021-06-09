eccda8dd7f1812ee283bd381ada435d804310ab7
withham <withham1@naver.com>
2021-06-09 17:36:12 +0900

    Create 가장 먼 노드.cpp

---
+++ programmers/Graph/가장 먼 노드.cpp
@@ -0,0 +1,47 @@
+/**
+ * problem : https://programmers.co.kr/learn/courses/30/lessons/49189
+ * time complexity : O(E)
+ * data structure : graph
+ */
+
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> edge) {
+    vector<vector<int>> edges(n);
+    for(auto vertex: edge) {
+        int u = vertex[0] - 1;
+        int v = vertex[1] - 1;
+        edges[u].push_back(v);
+        edges[v].push_back(u);
+    }
+    
+    queue<pair<int,int>> q;
+    q.push({0, 0});
+    vector<bool> visits(n, false);
+    visits[0] = true;
+        
+    int dist = 0;
+    int answer = 0;
+    while(!q.empty()) {
+        int now = q.front().first;
+        int cnt = q.front().second;
+        q.pop();
+        
+        if(dist != cnt) {
+            answer = 0;
+            dist = cnt;
+        }
+        answer++;
+        
+        for(auto next: edges[now]){
+            if(visits[next]) continue;
+            visits[next] = true;
+            q.push({next, cnt+1});
+        }
+    }
+    return answer;
+}
