13a277bf8f693ebbd95acf73e6c6ee7378b1465f
withham <withham1@naver.com>
2021-06-13 13:07:19 +0900

    Create 보석 쇼핑.cpp

---
+++ programmers/2020카카오인턴십/보석 쇼핑.cpp
@@ -0,0 +1,35 @@
+/**
+ * problem : https://programmers.co.kr/learn/courses/30/lessons/67258
+ * time complexity : O(N). N = |gems|
+ * algorithm : two-pointer
+ */
+
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<string> gems) {
+    set<string> gemKinds;
+    for(auto gem: gems) gemKinds.insert(gem);
+    
+    int n = gems.size();
+    map<string, int> cnts;
+    int left = 0;
+    vector<int> answer = {1, n};
+    set<string> exists;
+    for(int right = 0; right < n; right++) {
+        string gem = gems[right];
+        cnts[gem]++;
+        exists.insert(gem);
+        while(left < right && cnts[gems[left]] > 1) {
+            cnts[gems[left++]]--;
+        }
+        if(exists.size() == gemKinds.size() && 
+           right - left < answer[1] - answer[0]) 
+            answer = {left+1, right+1};
+    }
+    return answer;
+}
