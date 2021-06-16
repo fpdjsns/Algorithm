963c1e64a4f3ef80a03b42d6ba15c08fad8e4d3b
withham <withham1@naver.com>
2021-06-16 18:11:52 +0900

    Create 단속카메라.cpp

---
+++ programmers/Greedy/단속카메라.cpp
@@ -0,0 +1,31 @@
+/** 
+ * problem : https://programmers.co.kr/learn/courses/30/lessons/42884
+ * time complexity : O(NlogN)
+ * algorithm : two-pointer
+ */
+ 
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+    int n = routes.size();
+    sort(routes.begin(), routes.end());
+    
+    pair<int, int> cover = {-30001, -30001};
+    int answer = 0;
+    for(auto route: routes) {
+        int s = route[0];
+        int e = route[1];
+        // impossible cover
+        if(s > cover.second || e < cover.first ) {
+            answer++;
+            cover = {s , e};
+        } else {
+            cover = {max(cover.first, s), min(cover.second, e)};
+        }
+    }
+    return answer;
+}
