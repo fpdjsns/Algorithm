4965d24c508d46fbb582a065ed35f740c9871056
withham <withham1@naver.com>
2021-06-19 00:47:54 +0900

    Update 795. Number of Subarrays with Bounded Maximum.cpp

--- leetcode/medium/795. Number of Subarrays with Bounded Maximum.cpp
+++ leetcode/medium/795. Number of Subarrays with Bounded Maximum.cpp
@@ -3,7 +3,7 @@
  * time complexity : O(N). N = |nums|
  * algorithm : two-pointer
  */
- 
+
 class Solution {
 public:
     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
@@ -12,11 +12,12 @@ public:
         int answer = 0;
         for(int i=0; i<n; i++) {
             int num = nums[i];
-            if(num >= left) // possible
+            if(num >= left && num <= right) // possible
                 r = i;
             if(num > right) // impossible
                 l = i + 1;
-            answer += r - l + 1;
+            if(l <= r)
+                answer += r - l + 1;
         }
         return answer;
     }
