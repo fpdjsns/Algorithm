ace17a50e532551117b5d33255133cebc11c4117
withham <withham1@naver.com>
2021-09-24 20:35:33 +0900

    Create 1137. N-th Tribonacci Number.cpp

---
+++ leetcode/easy/1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,18 @@
+/**
+ * problem : https://leetcode.com/problems/n-th-tribonacci-number/
+ * time complexity : O(N)
+ */
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        long long a, b, c;
+        vector<int> arr = {0, 1, 1};
+        if(n < 3) return arr[n];
+        int i = 3;
+        for(; i<=n; i++){ 
+            arr[i%3] = arr[0] + arr[1] + arr[2];
+        }
+        return arr[(i-1)%3];
+    }
+};
