ff8739033af65d4e1ed9aee8de55cb3a714d3d55
withham <withham1@naver.com>
2021-10-05 20:25:23 +0900

    Create 2028. Find Missing Observations.cpp

---
+++ leetcode/medium/2028. Find Missing Observations.cpp
@@ -0,0 +1,26 @@
+/**
+ * problem : https://leetcode.com/problems/find-missing-observations/
+ * time complexity : 
+ */
+
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m = rolls.size();
+        int sum = mean * (n + m);
+        for(auto num: rolls) {
+            sum -= num;
+        }
+        
+        vector<int> answer(n, sum / n);
+        int avg = sum / n;
+        if(avg > 6 || avg < 1) return {};
+        
+        sum -= avg * n;
+        for(int i=0; i<sum; i++){
+            answer[i]++;
+            if(answer[i] > 6) return {};
+        }
+        return answer;
+    }
+};
