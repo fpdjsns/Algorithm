f041ea6d443936c9f882359648928a720beb9a0c
withham <withham1@naver.com>
2021-12-24 16:48:35 +0900

    Create 56. Merge Intervals.cpp

---
+++ leetcode/medium/56. Merge Intervals.cpp
@@ -0,0 +1,30 @@
+/**
+ * problem : https://leetcode.com/problems/merge-intervals/
+ * time complexity : O(N)
+ * algorithm : greedy
+ */
+ 
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> answer;
+        sort(intervals.begin(), intervals.end());
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for(int i=1; i<intervals.size(); i++){
+            // can't merge
+            if(intervals[i][0] > end) {
+                answer.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            } else { // can merge
+                // update end
+                end = max(end, intervals[i][1]);
+            }
+        }
+        answer.push_back({start, end});
+        
+        return answer;
+    }
+};
