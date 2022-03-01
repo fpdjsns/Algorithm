306a22c4acc2a0aa89ed8ed9b2c3cfca6f2f3651
withham <withham1@naver.com>
2022-03-01 14:24:19 +0900

    Create 338. Counting Bits.cpp

---
+++ leetcode/easy/338. Counting Bits.cpp
@@ -0,0 +1,18 @@
+/**
+ * problem : https://leetcode.com/problems/counting-bits/
+ * time complexity : O(N)
+ */
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> ans(num+1, 0);
+        for(int i=1;i<=num;i++){
+            if(i % 2 == 0) // even
+                ans[i] = ans[i/2];
+            else // odd
+                ans[i] = ans[i-1] + 1;
+        }
+        return ans;
+    }
+};
