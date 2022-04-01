301c8490d495255eb1c4312f70e8a487e89e238b
withham <withham1@naver.com>
2022-04-01 17:52:50 +0900

    Create 287. Find the Duplicate Number.cpp

---
+++ leetcode/medium/287. Find the Duplicate Number.cpp
@@ -0,0 +1,24 @@
+/**
+ * problem : https://leetcode.com/problems/find-the-duplicate-number/
+ * time complexity : O(N)
+ * algorithm : Floyd's cycle detection
+ */
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+            
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
