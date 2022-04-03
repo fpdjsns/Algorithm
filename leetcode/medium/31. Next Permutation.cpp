464e58e9764b5f1e441f672abb77b35175eab146
withham <withham1@naver.com>
2022-04-03 11:59:30 +0900

    Create 31. Next Permutation.cpp

---
+++ leetcode/medium/31. Next Permutation.cpp
@@ -0,0 +1,34 @@
+/**
+ * problem : https://leetcode.com/problems/next-permutation/
+ * time complexity : O(N)
+ */
+ 
+class Solution {
+    void reverse(vector<int>& nums, int index) {
+        int left = index;
+        int right = nums.size()-1;
+        while(left < right) {
+            iter_swap(nums.begin() + (left++), nums.begin() + (right--));
+        }
+    }
+    
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int index = n-1;
+        while(index >= 0 && nums[index] >= nums[min(index+1, n-1)]) {
+            index--;
+        }
+        cout << index << endl;
+        if(index == -1) {
+            reverse(nums, 0);
+        } else{
+            // nums[index] 보다 큰 값 중 가장 작은 값 찾기
+            int minNumIndex = index+1;
+            while(minNumIndex < n && nums[min(minNumIndex + 1, n-1)] > nums[index])
+                minNumIndex++;
+            iter_swap(nums.begin() + index, nums.begin() + min(n-1, minNumIndex));
+            reverse(nums, index+1);
+        }
+    }
+};
