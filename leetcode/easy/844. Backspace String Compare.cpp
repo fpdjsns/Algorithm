e7e5f6ea085d15aa624461339d185151e3ee19af
withham <withham1@naver.com>
2022-05-01 09:14:55 +0900

    Update 844. Backspace String Compare.cpp

--- leetcode/easy/844. Backspace String Compare.cpp
+++ leetcode/easy/844. Backspace String Compare.cpp
@@ -1,34 +1,24 @@
 /**
  * problem : https://leetcode.com/problems/backspace-string-compare/
  * time complexity : O(N+M) // N = |S|, M = |T|
- * data structure : stack
  */
-
 class Solution {
+    string getNewStr(string s){
+        string str;
+        for(auto c: s) {
+            if(c != '#') {
+                str.push_back(c);
+                continue;
+            }
+            if(str.empty()) continue;
+            str.pop_back();
+        }
+        return str;
+    }
 public:
-    bool backspaceCompare(string S, string T) {
-        stack<char> sSt;
-        stack<char> tSt;
-        for (int i = 0; i < S.size(); i++) {
-            if (S[i] == '#') {
-                if (!sSt.empty()) sSt.pop();
-            }
-            else
-                sSt.push(S[i]);
-        }
-        for (int i = 0; i < T.size(); i++) {
-            if (T[i] == '#'){
-                if(!tSt.empty()) tSt.pop();
-            }
-            else
-                tSt.push(T[i]);
-        }
-
-        while (!sSt.empty() && !tSt.empty()) {
-            if (sSt.top() != tSt.top())
-                return false;
-            sSt.pop(); tSt.pop();
-        }
-        return sSt.empty() && tSt.empty();
+    bool backspaceCompare(string s, string t) {
+        string newS = getNewStr(s);
+        string newT = getNewStr(t);
+        return newS == newT;
     }
 };
