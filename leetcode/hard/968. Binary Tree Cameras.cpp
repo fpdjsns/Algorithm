92d9ea5917316034b5ec6f486427527b3d88d15b
withham <withham1@naver.com>
2022-06-17 18:39:15 +0900

    Create 968. Binary Tree Cameras.cpp

---
+++ leetcode/hard/968. Binary Tree Cameras.cpp
@@ -0,0 +1,44 @@
+/**
+ * problem : https://leetcode.com/problems/binary-tree-cameras/
+ * time complexity : O(V+E)
+ * algorithm : DFS
+ */
+ 
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    const int NOT_MONITORED = 0;
+    const int MONITORED = 1;
+    const int SET_CAMERA = 2;
+    
+    int answer = 0;
+    int dfs(TreeNode* node) {
+        if(node == nullptr) return MONITORED;
+        
+        int left = dfs(node->left);
+        int right = dfs(node->right);
+        
+        int res = NOT_MONITORED;
+        if(left == NOT_MONITORED || right == NOT_MONITORED) {
+            res = SET_CAMERA;
+        } else if(left == SET_CAMERA || right == SET_CAMERA) {
+            res = MONITORED;
+        }
+        answer += (res == SET_CAMERA);
+        return res;
+    }
+public:
+    int minCameraCover(TreeNode* root) {
+        if(dfs(root) == NOT_MONITORED) answer++;
+        return max(1, answer);
+    }
+};
